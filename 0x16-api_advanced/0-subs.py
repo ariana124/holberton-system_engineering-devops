066db9d269b2b4f28d4d8971a9110c857552018d
ariana124 <ariana.bibiano@gmail.com>
2020-05-07 18:45:40 +0000

    uses Reddit's API to get subreddit subscriber count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Query that uses the Reddit API to get the number of subscribers for a
+given subreddit
+"""
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/" + subreddit
+    headers = {"User-Agent": "Mozilla 5.0"}
+
+    response = requests.get(url + "/about.json", headers=headers)
+
+    # If the page was found, then it will return the subscriber count.
+    if (response.status_code == 200):
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
+        return subscribers
+    else:
+        return 0

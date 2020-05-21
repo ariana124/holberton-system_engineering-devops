0da6638071d5eea575be5cf1a52529d74b1e3c5a
Ariana Bibiano <48872829+ariana124@users.noreply.github.com>
2020-05-20 20:26:03 -0700

    added comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Creates the phpp file with the contents of the php file
 file { '/var/www/html/wp-includes/class-wp-locale.phpp':
   ensure => file,
   source => '/var/www/html/wp-includes/class-wp-locale.php',
